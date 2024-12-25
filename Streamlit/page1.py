4a1c4d398efd6d72c6c18e8f1c1d8cd8e568943e
ChristelleTessier <christelle.tessier@eleve.ensai.fr>
2024-12-25 10:45:00 +0000

    debut streamlit

---
+++ Streamlit/page1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def page1():
+    st.header("Présentation des données brutes")
+    st.write("Ceci est le contenu de la page 1.")
---
+++ Streamlit/page2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def page2():
+    st.header("Visualisation des données")
+    st.write("Ceci est le contenu de la page 2.")
