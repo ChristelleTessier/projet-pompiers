607e07117f07e6c35270c81c00c708951021ffc2
ChristelleTessier <christelle.tessier@eleve.ensai.fr>
2025-02-20 08:18:24 +0100

    test pour streamlit

---
+++ Streamlit/old_streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+
+from page1 import page1
+from page2 import page2
+from page3 import page3
+from page4 import page4
+from page5 import page5
+from page6 import page6
+from page_intro import page_intro
+
+
+# Sidebar
+pages = {
+    "Presentation":page_intro,
+    "Presentation des données": page1,
+    "Visualisation des données": page2,
+    "Prétraitement des données": page3,
+    "Modélisation 1 - prédiction variable continue": page4,
+    "Modélisation 2 - prédiction variable discrète": page5,
+    "Conclusion et prespective": page6
+}
+
+# Barre latérale avec des boutons radio pour chaque page
+selected_page = st.sidebar.radio("Choisis une page", list(pages.keys()))
+
+# Appeler la fonction correspondant à la page sélectionnée
+pages[selected_page]()
+
+
+st.sidebar.write("""Auteurs :
+* Anne DUBOIS
+* Christelle TESSIER
+* Hao LA
+""")
+
